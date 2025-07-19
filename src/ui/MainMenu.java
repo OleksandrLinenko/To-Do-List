358e478f9e2e89a5547838809d9ce330dc76ad4a
oleksandrlinenko <oleksandrlinenko@localhost>
2025-07-19 13:47:32 +0200

    Add UI classes

---
+++ src/ui/MainMenu.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class MainMenu {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static MainMenu create() {
+        return new MainMenu();
+    }
+
+    public void show() {
+        boolean endProgram = false;
+        do {
+            menu();
+            int option = getOption();
+            endProgram = doOption(option);
+        } while (!endProgram);
+    }
+
+    public static void menu() {
+        Message.create().show("\nTo-Do-List\n");
+        Message.create().show("1. Show all uncompleted tasks\n");
+        Message.create().show("2. Show all uncompleted tasks from from specific date\n");
+        Message.create().show("3. Show all completed tasks\n");
+        Message.create().show("4. Show all uncompleted tasks from from specific date\n");
+        Message.create().show("5. Add new task\n");
+        Message.create().show("6. Mark uncompleted task as completed\n");
+        Message.create().show("0. End of program\n");
+    }
+
+    public static int getOption() {
+        Message.create().show("Pick the option: ");
+        int option;
+        try {
+            option = sc.nextInt();
+        } catch (InputMismatchException ex) {
+            return - 1;
+        }
+
+        return option;
+    }
+
+    public static boolean doOption(int option) {
+        switch (option) {
+            case 0:
+                return true;
+            case 1:
+
+                break;
+
+            default:
+                Message.create().show("Undefined option");
+        }
+
+        return false;
+    }
+
+}
